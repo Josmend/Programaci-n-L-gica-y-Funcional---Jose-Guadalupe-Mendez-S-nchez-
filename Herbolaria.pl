bf2680bc1c583c3b0575854983f6b405be5d1121
Méndez Sánchez José Guadalupe <98825870+Josmend@users.noreply.github.com>
2024-04-28 23:40:43 -0600

    Add files via upload

---
+++ Herbolaria.pl
@@ -0,0 +1,125 @@
+% Propiedades medicinales que tienen las plantas
+propiedades(geranio, astringentes).
+propiedades(geranio, estimulante).
+propiedades(geranio, descongestionante).
+
+
+propiedades(girasol, alivio_dolor).
+propiedades(girasol, relajante).
+propiedades(girasol, cicatrizante).
+
+
+
+propiedades(gingseng, astringentes).
+propiedades(gingsengc, estimulante).
+propiedades(gingseng, cicatrizante).
+propiedades(gingseng, alivio_dolor).
+propiedades(gingseng, relajante).
+propiedades(gingseng, antiinflamatorio).
+propiedades(gingseng, expectorante).
+propiedades(gingseng, etc).
+
+
+% Metodo de uso para las plantas
+metodo_de_uso(geranio, decocción).
+metodo_de_uso(geranio, inhalación).
+metodo_de_uso(geranio, ungüento).
+
+metodo_de_uso(girasol, decocción).
+metodo_de_uso(girasol, ingerir).
+
+metodo_de_uso(gingseng, ingerir).
+metodo_de_uso(gingseng, infusion).
+
+
+% Relación entre plantas y para que se pueden tratar
+cura(geranio, hemorragias).
+cura(geranio, nefritis).
+
+cura(girasol, hemicranea).
+cura(girasol, transtornos_nerviosos).
+cura(girasol, dolores).
+cura(girasol, pulmonia).
+cura(girasol, llagas).
+
+
+cura(gingseng, hemorragias).
+cura(gingseng, nefritis).
+cura(gingseng, hemicranea).
+cura(gingseng, transtornos_nerviosos).
+cura(gingseng, dolores).
+cura(gingseng, pulmonia).
+cura(gingseng, llagas).
+cura(gingseng, algunos_más).
+
+
+
+% Fortalecimiento del organismo
+fortalece_sistema_digestivo(geranio).
+fortalece_sistema_digestivo(gingseng).
+fortalece_sistema_cognitivo(gingseng).
+
+%----------------------------------------------------------------------------------------------------------------
+
+% Propiedades medicinales que tienen las plantas
+propiedades(jengibre, antiinflamatorio).
+propiedades(jengibre, digestivo).
+propiedades(jengibre, estimulante).
+
+propiedades(menta, digestivo).
+propiedades(menta, relajante).
+propiedades(menta, alivio_dolor).
+
+propiedades(eucalipto, descongestionante).
+propiedades(eucalipto, expectorante).
+
+propiedades(calendula, cicatrizante).
+propiedades(calendula, antiinflamatorio).
+
+
+
+% Metodo de uso para las plantas
+metodo_de_uso(jengibre, infusion).
+metodo_de_uso(jengibre, cataplasma).
+metodo_de_uso(jengibre, decocción).
+
+metodo_de_uso(menta, compresa).
+metodo_de_uso(menta, infusion).
+metodo_de_uso(menta, aceite_esencial).
+
+
+metodo_de_uso(eucalipto, pomada).
+metodo_de_uso(eucalipto, inhalación).
+
+metodo_de_uso(calendula, ungüento).
+metodo_de_uso(calendula, compresa).
+
+
+% Relación entre plantas y para que se pueden tratar
+cura(jengibre, gripe).
+cura(jengibre, dolor_estomacal).
+cura(jengibre, nauseas).
+
+cura(menta, indigestión).
+cura(menta, dolor_de_cabeza).
+cura(menta, estrés).
+cura(menta, insomnio).
+
+cura(eucalipto, congestión_respiratoria).
+cura(eucalipto, tos).
+cura(eucalipto, sinusitis).
+
+cura(calendula, heridas).
+cura(calendula, inflamación).
+cura(calendula, irritación_piel).
+cura(calendula, quemaduras).
+
+% Fortalecimiento del organismo
+fortalece_sistema_digestivo(menta).
+fortalece_sistema_respiratorio(eucalipto).
+
+
+
+
+
+
